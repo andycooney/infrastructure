fcdd6cd1ef1e2bb9e24282fc92752df01409161d
andycooney <andycooney@gmail.com>
2025-04-11 16:47:48 -0400

    chore: rebuild

--- scripts/bootstrap-apps.sh
+++ scripts/bootstrap-apps.sh
@@ -131,7 +131,7 @@ function main() {
     # Apply resources and Helm releases
     wait_for_nodes
     apply_namespaces
-    apply_sops_secrets
+    # apply_sops_secrets
     apply_crds
     apply_helm_releases
 
